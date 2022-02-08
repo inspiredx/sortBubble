d1e51afbeec53403c4a2f57a17b05510d63940f5
inspiredx <georgiykapustinx@gmail.com>
2022-02-08 16:22:44 +0300

    1

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // задаем случайый массив по методу .map (задает 3 значения) рандомный
 /*const array =  Array(20).fill(null).map(() => Math.floor
 (Math.random() *2000) - 1000)*/
@@ -26,24 +25,23 @@
   696]*/
 
 const sorted = bubbleSort(
-Array(20).fill(null).map(() => Math.floor
-(Math.random() *2000) - 1000)
+  Array(20).fill(null).map(() => Math.floor(Math.random() * 2000) - 1000)
 )
 
 console.log(sorted)
 
-function bubbleSort (array) {
-  for (let n = 0; n < array.length; n++) {  //создаем цикл для цикла замены
-   for (let i = 0; i < array.length - 1 - n; i++) { //создаем цикл для замены + добавляем -n чтобы цикл каждый раз не проходил уже отфильтрованное
-     //если нынешнее айное число больше ай+1, тогда
-     if (array [i] > array [i + 1]) { 
-     // 3 строчки алгоритм для того чтобы поменять числа местами 
-       const buff =  array[i]
-       array[i] = array[i + 1]
-       array[i + 1] = buff
-     }
+function bubbleSort(array) {
+  for (let n = 0; n < array.length; n++) { //создаем цикл для цикла замены
+    for (let i = 0; i < array.length - 1 - n; i++) { //создаем цикл для замены + добавляем -n чтобы цикл каждый раз не проходил уже отфильтрованное
+      //если нынешнее айное число больше ай+1, тогда
+      if (array[i] > array[i + 1]) {
+        // 3 строчки алгоритм для того чтобы поменять числа местами 
+        const buff = array[i]
+        array[i] = array[i + 1]
+        array[i + 1] = buff
+      }
 
-     }
+    }
 
   }
 
